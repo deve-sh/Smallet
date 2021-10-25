70e5ee00db6e815384fa3b63570815733871cfbc
Devesh Kumar <devesh2027@gmail.com>
2021-10-25 20:22:09 +0530

    Added firebase auth handler

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
+import { useEffect } from "react";
+import auth, { getToken } from "../firebase/authentication";
+
 import useStore from "../hooks/useStore";
 
-const App = ({ Component, pageProps }) => {
-	useStore();
-	return <Component {...pageProps} />;
+const App = ({ Component: Page, pageProps }) => {
+	const setUser: Function = useStore((store) => store.setUser);
+
+	useEffect(() => {
+		auth.onAuthStateChanged((userFromFirebase) => {
+			let user = null;
+			if (userFromFirebase) {
+				user = {
+					displayName: userFromFirebase.displayName,
+					email: userFromFirebase.email,
+					photoURL: userFromFirebase.photoURL,
+					emailVerified: userFromFirebase.emailVerified,
+					phoneNumber: userFromFirebase.phoneNumber,
+				};
+				getToken();
+			}
+            console.log(setUser);
+			setUser(user);
+		});
+	}, []);
+
+	return <Page {...pageProps} />;
 };
 
 export default App;
