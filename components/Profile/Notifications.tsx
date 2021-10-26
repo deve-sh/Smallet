dbd5bb45b538ed767a5434e0eb2a9302dab407e8
Devesh Kumar <devesh2027@gmail.com>
2021-10-26 19:46:28 +0530

    Notifications Dropdown setup

--- components/Profile/Notifications.tsx
+++ components/Profile/Notifications.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
-import {
-	IconButton,
-	Menu,
-	MenuButton,
-	MenuList,
-	MenuItem,
-} from "@chakra-ui/react";
+import { NotificationContent } from "../../@types";
+
+import styled from "@emotion/styled";
+import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import { MdNotifications, MdNotificationImportant } from "react-icons/md";
 
 import useStore from "../../hooks/useStore";
@@ -15,9 +12,20 @@ import { getUserNotifications } from "../../API/notifications";
 
 import toasts from "../../utils/toasts";
 
+const NotificationPop = styled.div`
+	height: 0.75rem;
+	width: 0.75rem;
+	border-radius: 50%;
+	background: orangered;
+	position: absolute;
+	right: -0.25rem;
+	top: -0.25rem;
+`;
+
 const UserNotifications = () => {
 	const user = useStore((state) => state.user);
 	const [notifications, setNotifications] = useState([]);
+	const [hasUnreadNotifications, setHasUnreadNotifications] = useState(false);
 
 	useEffect(() => {
 		if (user) {
@@ -25,6 +33,9 @@ const UserNotifications = () => {
 			getUserNotifications(userId, null, (err, notificationsFetched) => {
 				if (err) return toasts.generateError(err);
 				setNotifications(notificationsFetched || []);
+				setHasUnreadNotifications(
+					notificationsFetched.some((entry: NotificationContent) => !entry.read)
+				);
 			});
 		}
 	}, []);
@@ -33,12 +44,14 @@ const UserNotifications = () => {
 		<>
 			<Menu>
 				<MenuButton
-					variant="ghost"
-					colorScheme="gray"
-					as={IconButton}
-					icon={<MdNotifications />}
+					variant={hasUnreadNotifications ? "outline" : "ghost"}
+					colorScheme={hasUnreadNotifications ? "red" : "gray"}
+					as={Button}
 					aria-label="Toggle Notifications"
-				/>
+				>
+					<MdNotifications />
+					{hasUnreadNotifications && <NotificationPop />}
+				</MenuButton>
 				<MenuList minHeight="50vh" minWidth="35vw">
 					{notifications.length ? (
 						notifications.map((notification) => (
