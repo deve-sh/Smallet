97597a23d470da81828802392ee44e7c509c8af1
Devesh Kumar <devesh2027@gmail.com>
2021-10-26 09:46:47 +0530

    Added more info to user auth listener

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,11 +11,18 @@ const App = ({ Component: Page, pageProps }) => {
 			let user = null;
 			if (userFromFirebase) {
 				user = {
+					uid: userFromFirebase.uid,
+					isAnonymous: userFromFirebase.isAnonymous,
 					displayName: userFromFirebase.displayName,
 					email: userFromFirebase.email,
 					photoURL: userFromFirebase.photoURL,
 					emailVerified: userFromFirebase.emailVerified,
 					phoneNumber: userFromFirebase.phoneNumber,
+					lastSignInTime: userFromFirebase.metadata.lastSignInTime,
+					creationTime: userFromFirebase.metadata.creationTime,
+					providerData: JSON.parse(
+						JSON.stringify(userFromFirebase.providerData)
+					),
 				};
 				getToken();
 			}
