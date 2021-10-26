8e2b59cd3b25013309222472bc1de3241a1cce95
Devesh Kumar <devesh2027@gmail.com>
2021-10-26 18:26:42 +0530

    Added notification related APIs

---
+++ API/notifications.ts
@@ -0,0 +1,85 @@
+import db, { firestore } from "../firebase/firestore";
+import auth from "../firebase/authentication";
+
+interface NotificationContent {
+	text: string;
+	type: "wallet" | "profile";
+	read: boolean;
+	url?: string;
+	image?: string;
+	amountChange?: number;
+}
+
+export const getUserNotifications = async (
+	userId: string,
+	startAfter: any,
+	callback: (errorMessage: string | null, notifications?: Array<object>) => any
+) => {
+	try {
+		let userNotificationsRef = db
+			.collection("notifications")
+			.where("user", "==", userId)
+			.where("read", "==", false);
+
+		if (startAfter)
+			userNotificationsRef = userNotificationsRef.startAfter(startAfter);
+
+		userNotificationsRef = userNotificationsRef
+			.limit(10)
+			.orderBy("createdAt", "desc");
+
+		const userNotifications = await userNotificationsRef.get();
+		return callback(
+			null,
+			userNotifications.docs.map((doc) => ({
+				...doc.data(),
+				id: doc.id,
+			}))
+		);
+	} catch (err) {
+		if (process.env.NODE_ENV !== "production") console.log(err);
+		return callback(err.message);
+	}
+};
+
+export const markNotificationsAsRead = async (
+	notificationIds: [string],
+	callback: (errorMessage: string | null) => any
+) => {
+	try {
+		const batch = db.batch();
+
+		for (let id of notificationIds) {
+			batch.update(db.collection("notifications").doc(id), {
+				updatedAt: firestore.FieldValue.serverTimestamp(),
+				read: true,
+			});
+		}
+
+		await batch.commit();
+	} catch (err) {
+		if (process.env.NODE_ENV !== "production") console.log(err);
+		return callback(err.message);
+	}
+};
+
+export const createUserNotification = async (
+	userId: string,
+	notificationContent: NotificationContent,
+	callback: (errorMessage: string | null) => any
+) => {
+	try {
+		const notificationDoc = db.collection("notifications").doc();
+		await notificationDoc.set({
+			...notificationContent,
+			user: userId,
+			createdAt: firestore.FieldValue.serverTimestamp(),
+			updatedAt: firestore.FieldValue.serverTimestamp(),
+			createdBy: auth.currentUser?.uid,
+		});
+		return callback(null);
+	} catch (err) {
+		if (process.env.NODE_ENV !== "production") console.log(err);
+		return callback(err.message);
+	}
+};
