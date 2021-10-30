856f5290867bb2c35899f4d4a676c419188580d1
Devesh Kumar <devesh2027@gmail.com>
2021-10-30 16:52:54 +0530

    Small fix for create wallet money transaction

--- pages/api/createWalletAddMoneyTransaction.ts
+++ pages/api/createWalletAddMoneyTransaction.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import razorpay from "../../utils/razorpay";
 import admin from "../../firebase/admin";
 import { validateIdToken } from "../../utils/firebaseAdminUtils";
-import { firestore } from "../../firebase/firestore";
 
 export default async function createWalletAddMoneyTransaction(
 	req: NextApiRequest,
@@ -35,52 +34,51 @@ export default async function createWalletAddMoneyTransaction(
 			.collection("wallets")
 			.doc(decodedToken.uid);
 
-		razorpay.orders.create(
-			{
-				amount,
-				currency: "INR",
-				receipt: transactionRef.id,
-				notes: {
-					transactionId: transactionRef.id,
-					user: decodedToken.uid,
-				},
+		const order = await razorpay.orders.create({
+			amount,
+			currency: "INR",
+			receipt: transactionRef.id,
+			notes: {
+				transactionId: transactionRef.id,
+				user: decodedToken.uid,
 			},
-			async (err, order) => {
-				if (err) return error(500, err.message);
-				const orderRef = admin.firestore().collection("orders").doc(order.id);
-				batch.set(transactionRef, {
-					user: decodedToken.uid,
-					amount,
-					wallet: decodedToken.uid,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-					status: "pending",
-					type: "wallet_topup",
-					order: order.id,
-				});
-				batch.set(orderRef, {
-					...order,
-					user: decodedToken.uid,
-					amount,
-					wallet: decodedToken.uid,
-					transaction: transactionRef.id,
-					createdAt: new Date(),
-					updatedAt: new Date(),
-				});
-				batch.update(userRef, {
-					nTransactions: firestore.FieldValue.increment(1),
-					updatedAt: new Date(),
-				});
-				batch.update(walletRef, {
-					nTransactions: admin.firestore.FieldValue.increment(1),
-					updatedAt: new Date(),
-				});
-				await batch.commit();
-				return res
-					.status(201)
-					.json({ message: "Created Order Successfully", order });
-			}
-		);
+		});
+
+		if (order) {
+			const orderRef = admin.firestore().collection("orders").doc(order.id);
+			batch.set(transactionRef, {
+				user: decodedToken.uid,
+				amount,
+				wallet: decodedToken.uid,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+				status: "pending",
+				type: "wallet_topup",
+				order: order.id,
+			});
+			batch.set(orderRef, {
+				...order,
+				user: decodedToken.uid,
+				amount,
+				wallet: decodedToken.uid,
+				transaction: transactionRef.id,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			});
+			batch.update(userRef, {
+				nTransactions: admin.firestore.FieldValue.increment(1),
+				updatedAt: new Date(),
+			});
+			batch.update(walletRef, {
+				nTransactions: admin.firestore.FieldValue.increment(1),
+				updatedAt: new Date(),
+			});
+			await batch.commit();
+			return res
+				.status(201)
+				.json({ message: "Created Order Successfully", order });
+		}
+		return error(500, "Payment could not be created.");
 	} catch (err) {
 		console.log(err);
 		return error(500, err.message);
