2adf4c38bc4bd06bd56c510b2a6fb0f0fc38920e
Devesh Kumar <devesh2027@gmail.com>
2021-10-30 12:26:13 +0530

    Added server-side and client-side checks to setupProtectedRoute

--- utils/setupProtectedRoute.ts
+++ utils/setupProtectedRoute.ts
@@ -1,21 +1,32 @@
+import Router from "next/router";
+import Cookie from "js-cookie";
+
 export default function setupProtectedRoute(callback?: (ctx: any) => any) {
 	return async function getInitialProps(ctx) {
 		const { req, res } = ctx;
+		const isServer = typeof window === "undefined";
 
-		if (req.cookies.accessToken) {
-			// Allowed
-			if (callback) {
-				const valuesToReturnAsInitialProps = await callback(ctx);
-				return valuesToReturnAsInitialProps;
+		if (isServer) {
+			if (req?.cookies?.accessToken) {
+				// Allowed
+				if (callback) {
+					const valuesToReturnAsInitialProps = await callback(ctx);
+					return valuesToReturnAsInitialProps;
+				}
+				return { protected: true };
 			}
-			return { protected: true };
+			// Redirect to home page.
+			res?.writeHead?.(302, {
+				Location: "/",
+			});
+			res?.end?.();
+		} else {
+			if (!Cookie.get("accessToken")) {
+				Router.push("/");
+				return { protected: true };
+			}
 		}
-		// Redirect to home page.
-		res.writeHead(302, {
-			Location: "/",
-		});
-		res.end();
-		return {};
+		return { protected: false };
 	};
 }
 
