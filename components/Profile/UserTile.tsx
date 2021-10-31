89b79895a2588e1ebfc3d32173a661a40016957f
Devesh Kumar <devesh2027@gmail.com>
2021-10-31 23:51:57 +0530

    Added reusable UserTile component

---
+++ components/Profile/UserTile.tsx
@@ -0,0 +1,33 @@
+import { Box, HStack, Avatar, Text } from "@chakra-ui/react";
+
+const UserTile = ({ user, onClick, title, border }) => {
+	return (
+		<Box
+			onClick={onClick}
+			shadow="md"
+			p={3}
+			borderRadius="0.25rem"
+			borderWidth={1}
+			cursor="pointer"
+			width="100%"
+			title={title}
+			border={border}
+		>
+			<HStack>
+				<Box>
+					<Avatar name={user.displayName} src={user.photoURL} />
+				</Box>
+				<Box flex="8">
+					<Text fontSize="md" fontWeight={500}>
+						{user?.displayName}
+					</Text>
+					<Text fontSize="sm" color="gray">
+						{user.phoneNumber} {user.email}
+					</Text>
+				</Box>
+			</HStack>
+		</Box>
+	);
+};
+
+export default UserTile;
