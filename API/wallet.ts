87f245416de11a7a902eb4c40282a5b1230c4497
Devesh Kumar <devesh2027@gmail.com>
2021-10-31 14:11:57 +0530

    Added wallet money transfer creating API function

--- API/wallet.ts
+++ API/wallet.ts
@@ -22,3 +22,26 @@ export const createAddMoneyToWalletTransaction = async (
 		return callback(err.message);
 	}
 };
+
+export const createWalletMoneyTransferTransaction = async (
+	amount: number,
+	userToTransferTo: string,
+	callback: (errorMessage: string | null, orderInfo?: any) => any
+) => {
+	try {
+		if (!amount || !userToTransferTo) return callback("Amount or user invalid");
+		request(
+			"/api/transferMoneyToOtherWallet",
+			{ amount, userToTransferTo },
+			{ headers: { authorization: await getToken() } },
+			"post",
+			(error, response) => {
+				if (error) return callback(error);
+				return callback(null, response);
+			}
+		);
+	} catch (err) {
+		if (process.env.NODE_ENV !== "production") console.log(err);
+		return callback(err.message);
+	}
+};
